19a8857cf30ee4690d5803679886eef0175a481f
Yuichi Yazaki <visualizingjp@gmail.com>
2025-10-14 16:32:26 +0900

    deploy

---
+++ rollup.config.js
@@ -0,0 +1,68 @@
+import resolve from '@rollup/plugin-node-resolve';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import replace from '@rollup/plugin-replace';
+import markdown from '@jackfranklin/rollup-plugin-markdown';
+import css from 'rollup-plugin-import-css';
+import copy from 'rollup-plugin-copy';
+import dotenv from 'rollup-plugin-dotenv';
+
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+    sourcemap: true,
+    strict: false
+  },
+  plugins: [
+    dotenv(),
+    commonjs({
+      include: ['./src/**/*', './node_modules/**/*'],
+      transformMixedEsModules: true
+    }),
+
+    replace({
+      'require.main === module': 'false', // jsonhint export quirk
+      preventAssignment: true,
+      'process.env.MAPBOX_ACCESS_TOKEN': JSON.stringify(process.env.MAPBOX_ACCESS_TOKEN)
+    }),
+
+    resolve({
+      browser: true,
+      preferBuiltins: true
+    }),
+
+    json(),
+
+    nodePolyfills(),
+
+    markdown(),
+
+    css({
+      output: './dist/css/bundle.css',
+      minify: production
+    }),
+
+    copy({
+      targets: [
+        {
+          src: 'node_modules/@fortawesome/fontawesome-free/webfonts',
+          dest: './dist'
+        },
+        {
+          src: 'node_modules/@mapbox/maki/icons',
+          dest: './dist'
+        },
+        {
+          src: 'img/marker-chars/*.svg',
+          dest: './dist/icons'
+        }
+      ]
+    })
+  ]
+};
